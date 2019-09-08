3f7565ee2eb484300bae2b80e4052a0c555ab6c7
Artromterra <54408191+Artromterra@users.noreply.github.com>
2019-09-09 00:49:07 +0700

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,57 @@
+const numDivs = 36;
+const maxHits = 10;
+
+
+let hits = 0;
+let firstHitTime = 0;
+let misses = 0;
+
+
+function round() {
+  $('.target').removeClass('target'); 
+  $('.miss').removeClass('miss'); 
+  $('#win-message').hide();
+
+  let divSelector = randomDivId();
+  $(divSelector).addClass('target')
+  $(divSelector).text(hits + 1)
+  // console.log(divSelector);
+
+  if (hits === 1) { firstHitTime = getTimestamp(); } 
+  if (hits === maxHits) { endGame(); } 
+}
+
+function endGame() {
+  $('.game-field').hide();
+
+  let totalPlayedMillis = getTimestamp() - firstHitTime;
+  let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
+  $('#total-time-played').text(totalPlayedSeconds);
+  $('#total-miss').text(misses);
+  $('#win-message').show();
+}
+
+function handleClick(event) {
+  let target = $(event.target)
+  if (target.hasClass('target')) {
+    hits = hits + 1;
+    target.text('');
+    round();
+  } else { $(event.target).addClass('miss');
+      misses = misses + 1;
+      // console.log(misses);
+    }
+    
+}
+
+
+function init() {
+    round();
+
+    $('.game-field').click(handleClick);
+    $('#button-reload').click(function() {
+      location.reload();
+  });
+}
+
+$(document).ready(init);
